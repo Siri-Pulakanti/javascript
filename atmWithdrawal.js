e9b89c102395002df49aaabdfef85b30c715c48e
Siri Chandana Pulakanti <sirichandanapulakanti@Siris-MacBook-Air.local>
2025-03-02 16:19:15 +0530

    added daily transaction limit

--- atmWithdrawal.js
+++ atmWithdrawal.js
@@ -6,14 +6,21 @@ if reqmoney>current bal==insufficient bal
 else trans of amt xxx is success remaining balance is yyy
 */
 let total = 10000;
+let dailyTranslimit = 5000;
 function withDrawMoney(reqMoney) {
-  if (total >= reqMoney) {
+  if (total >= reqMoney && reqMoney <= dailyTranslimit) {
     total = total - reqMoney;
     console.log(
       "The transaction of",
       reqMoney,
       "is successful remianing balance is",
       total
+    );
+    dailyTranslimit -= reqMoney;
+  } else if (total >= reqMoney && reqMoney > dailyTranslimit) {
+    console.log(
+      "The requested amout is exceeding daily transaction limit",
+      dailyTranslimit
     );
   } else {
     console.log("Insufficient Balance and your current balance is", total);
@@ -22,5 +29,6 @@ function withDrawMoney(reqMoney) {
 
 withDrawMoney(50);
 withDrawMoney(250);
+withDrawMoney(4900);
 withDrawMoney(750);
 withDrawMoney(15000);
