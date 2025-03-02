de87096fcf9fbd5f2dc6d0770e7d250701c67912
Siri Chandana Pulakanti <sirichandanapulakanti@Siris-MacBook-Air.local>
2025-03-02 21:33:40 +0530

    forLoop.js

---
+++ forLoop.js
@@ -0,0 +1,17 @@
+/*function greet(n) {
+  for (i = 1; i <= n; i++) {
+    console.log("Hello ");
+  }
+}
+
+greet(3);
+
+*/
+
+function packGiftBox() {
+  for (i = 1; i <= 3; i++) {
+    console.log("Pcaking gift box", i);
+  }
+}
+
+packGiftBox();
