5c56f968b2a9bd3793bdcdd4aa5319b3c6ba84c7
Siri Chandana Pulakanti <sirichandanapulakanti@Siris-MacBook-Air.local>
2025-03-02 16:45:50 +0530

    added rate parameter

--- mobileBalanceTracking.js
+++ mobileBalanceTracking.js
@@ -1,6 +1,6 @@
 /*
 mob bal=600
-rate 1rup/min
+rate 5rup/min
 makecall function(time in min)
 call success you are charged xx and balance is yy
 call failed insufficient bal, available bal yy
@@ -8,9 +8,9 @@ call failed insufficient bal, available bal yy
 */
 
 let mobileBal = 600;
-function makeCall(duration) {
-  if (mobileBal >= duration) {
-    mobileBal -= duration;
+function makeCall(duration, rate) {
+  if (mobileBal >= rate * duration) {
+    mobileBal -= rate * duration;
     console.log(
       "The call is successful you are charged",
       duration,
@@ -25,7 +25,8 @@ function makeCall(duration) {
   }
 }
 
-makeCall(55);
-makeCall(78);
-makeCall(780);
-makeCall(150);
+makeCall(55, 0.9);
+makeCall(31, 0.5);
+makeCall(78, 0.08);
+makeCall(780, 0.06);
+makeCall(150, 0.000004);
