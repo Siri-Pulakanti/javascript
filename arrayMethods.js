c40cc585753400ce84943f682e0581c417bf9454
Siri-Pulakanti <pulakantisiri1@gmail.com>
2025-03-09 20:27:34 +0530

    for-each,map

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,4 +1,4 @@
-// let numbers = [1, 2, 4, 15, 66, 53, 78, 94, 3];
+let numbers = [1, 2, 4, 15, 66, 53, 78, 94, 3];
 
 // const evens = numbers.filter((num) => num % 2 == 0);
 // console.log(evens);
@@ -20,8 +20,23 @@ let items = [
 // const itemsWithA = items.filter((item) => item.toLowerCase().includes(myChar));
 // console.log(itemsWithA);
 
-items.sort();
-console.log(items);
+// items.sort();
+// console.log(items);
 
-items.reverse();
-console.log(items);
+// items.reverse();
+// console.log(items);
+
+// items.forEach((item) => console.log(item));
+// numbers.forEach((num) => console.log(num * 2));
+
+// let age = 12;
+// // while (age < 18) {
+// //   console.log("you are not allowed tovote", age);
+// //   age++;
+// // }
+// do {
+//   console.log("you are not allowed tovote", age);
+//   age++; // Code to be executed
+// } while (age < 18);
+
+items.map((item) => console.log(item));
