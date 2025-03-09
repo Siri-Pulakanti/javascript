8b47d7037844164710e36e02ad39c32949dfd87a
Siri-Pulakanti <pulakantisiri1@gmail.com>
2025-03-09 20:03:45 +0530

    filter

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,6 +1,27 @@
-let numbers = [1, 2, 4, 15, 66, 53, 78, 94, 3];
-let evenNumbers = numbers.filter(function (num) {
-  return num % 2 == 0;
-});
-console.log(evenNumbers);
-console.log(numbers);
+// let numbers = [1, 2, 4, 15, 66, 53, 78, 94, 3];
+
+// const evens = numbers.filter((num) => num % 2 == 0);
+// console.log(evens);
+
+// const odds = numbers.filter((num) => num % 2 == 1);
+// console.log(odds);
+
+let items = [
+  "Apple",
+  "Mango",
+  "Grapes",
+  "Tomato",
+  "Carrot",
+  "Potato",
+  "Berry",
+  "Banana",
+];
+// let myChar = "a";
+// const itemsWithA = items.filter((item) => item.toLowerCase().includes(myChar));
+// console.log(itemsWithA);
+
+items.sort();
+console.log(items);
+
+items.reverse();
+console.log(items);
