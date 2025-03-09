cb1b91e3ce11fb683bbbef94b5645f574e991abb
Siri Chandana Pulakanti <sirichandanapulakanti@Siris-MacBook-Air.local>
2025-03-09 11:06:25 +0530

    introduced nested loop

--- serveTable.js
+++ serveTable.js
@@ -1,18 +1,9 @@
-//THIS FUNCTION SERVES A PARTICULAR DISH TO A PARTICULAR TABLE
-function serveDish(dishNumber, tableName) {
-  console.log("Serving the dish", dishNumber, "to table", tableName);
-}
-
-//this function serves a particular number of dishes to a particular table
-function serveTable(numOfDishes, tableName) {
-  for (let i = 1; i <= numOfDishes; i++) {
-    serveDish(i, tableName);
-  }
-}
 //this function serves a particular number of dishes to all tables
 function serveRestaurent(numOfDishes, numOfTables) {
   for (let i = 1; i <= numOfTables; i++) {
-    serveTable(numOfDishes, i);
+    for (let j = 1; j <= numOfDishes; j++) {
+      console.log("Serving the dish", j, "to table", i);
+    }
   }
 }
 
